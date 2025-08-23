3bec9e24f9d10cac0e04694a1d307b1fb972501c
Arslan <arslanmian604@gmail.com>
2025-08-23 20:37:35 +0500

    Add project files.

---
+++ LoginformSession/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LoginformSession.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
